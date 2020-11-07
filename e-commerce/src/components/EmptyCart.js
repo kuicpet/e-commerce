e8051e1bd4f00b42c199713d323c856c418ed61e
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-07 02:52:52 +0100

    ft--empty cart

--- e-commerce/src/components/EmptyCart.js
+++ e-commerce/src/components/EmptyCart.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
 
 export default function EmptyCart() {
     return (
         <div className="container mt-5">
-            <div className="row col-10 mx-auto text-center text-title">
-                <h3>your cart is currently empty</h3>
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-title">
+                    <h3>your cart is currently empty</h3>
+                    <div className="cart-icon2">
+                        <FiShoppingCart/>
+                    </div>
+                    <Link to="/">
+                        <button className="text-center text-capitalize mx-auto px-4 py-1 shop">start shopping</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
